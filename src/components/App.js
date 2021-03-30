8e6f12cd0ae6dea35514caed5d109436934b47c5
Anthony Citraro <anthonycitraro12@gmail.com>
2021-03-29 20:17:24 -0400

    Added front end deposit

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,6 @@ import dbank from '../dbank.png';
 import Web3 from 'web3';
 import './App.css';
 
-//h0m3w0rk - add new tab to check accrued interest
-
 class App extends Component {
 
   async componentWillMount() {
@@ -19,24 +17,28 @@ class App extends Component {
       const web3 = new Web3(window.ethereum);
       const netId = await web3.eth.net.getId();
       const accounts = await web3.eth.getAccounts();
-      if(typeof accounts[4] !== 'undefined'){
-        const balance = await web3.eth.getBalance(accounts[4]);
-        this.setSate({ account: accounts[0], balance: balance, web3: web3});
+      if(typeof accounts[0] !== 'undefined'){
+        const balance = await web3.eth.getBalance(accounts[0]);
+        this.setState({ account: accounts[0], balance: balance, web3: web3});
       }else{
         window.alert('Please login with MetaMask')
+      }
+
+      try{
+        const token = new web3.eth.Contract(Token.abi,Token.networks[netId].address);
+        const dbank = new web3.eth.Contract(dBank.abi,dBank.networks[netId].address);
+        const dBankAddress = dBank.networks[netId].address;
+        console.log(dBank.networks[netId].address);
+        this.setState({token: token, dbank: dbank, dBankAddress: dBankAddress});
+      }catch(e){
+        console.log('Error',e);
+        window.alert('Contracts not deployed to the current network');
       }
 
     }else{
       window.alert('Please install MetaMask');
     }
 
-      //assign to values to variables: web3, netId, accounts
-
-      //check if account is detected, then load balance&setStates, elsepush alert
-
-      //in try block load contracts
-
-    //if MetaMask not exists push alert
   }
 
   async deposit(amount) {
@@ -78,15 +80,37 @@ class App extends Component {
         </nav>
         <div className="container-fluid mt-5 text-center">
         <br></br>
-          <h1>{/*add welcome msg*/}</h1>
-          <h2>{/*add user address*/}</h2>
+          <h1>Welcome to dBank!</h1>
+          <h2>{this.state.account}</h2>
           <br></br>
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
               <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-                {/*add Tab deposit*/}
-                {/*add Tab withdraw*/}
+                <Tab eventKey="deposit" title="Deposit">
+                    <div>
+                      <br></br>
+                      How much do you want to deposit?
+                      <br></br>
+                      (min. amount = 0.01 ETH)
+                      <br></br>
+                      <form onSubmit={(e) => {
+
+                      }}>
+                        <div className='form-group mr-sm-2'>
+                          <br></br>
+                          <input/>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Deposit Now!</button>
+                      </form>
+                    </div>
+                </Tab>
+                <Tab eventKey="withdraw" title="Withdraw">
+                    <div>
+                      <br></br>
+                      Do you want to withdraw + take interst?
+                    </div>
+                </Tab>
               </Tabs>
               </div>
             </main>
